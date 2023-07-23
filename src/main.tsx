73a5e0170440b572bc608a25ea9b7427f6ac8370
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-07-24 01:02:07 +0200

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import './index.css'
 // Import our custom CSS
 import '../src/scss/style.css'
+import Contacts from './components/contacts/Contacts.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Contacts />
+  // <React.StrictMode>
+    // <App />
+  // </React.StrictMode>,
 )
